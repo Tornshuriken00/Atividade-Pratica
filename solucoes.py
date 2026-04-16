86a8c584853c4a3cd0445c989fb668f708e776ff
Tornshuriken00 <99892908+Tornshuriken00@users.noreply.github.com>
2026-04-16 10:11:52 -0300

    Create solucoes.py

---
+++ solucoes.py
@@ -0,0 +1,9 @@
+def sao_anagramas(string1, string2):
+  # TODO: implementar a logica
+
+def citra_de_cesar(texto, deslocamento):
+  # TODO: implementar a lógica
+
+def valida_cpf(cpf_string):
+  # TODO: implementar a lógica
+  pass
